c034cf0d9e8dafe7d16222d88ff70acf2ad18a8e
Pablo Panero <ppanero@cern.ch>
2019-11-05 10:26:37 +0100

    tests: add travis and required scripts to run tests

---
+++ run-tests.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+#
+# Copyright (C) 2019 CERN.
+# Copyright (C) 2019 Northwestern University.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+# quit on errors:
+set -o errexit
+
+# quit on unbound symbols:
+set -o nounset
+
+WORKDIR=$(mktemp -d)
+
+finish() {
+    echo "Cleaning up."
+    docker-compose -f docker-compose.full.yml down --volumes --remove-orphans &
+    pipenv --rm || true
+    rm -rf "${WORKDIR}"
+}
+
+trap finish EXIT
+
+PROJECT_NAME="my-site"
+
+cookiecutter --no-input -o "$WORKDIR" . \
+    project_name=${PROJECT_NAME} \
+    database=${COOKIECUTTER_DATABASE:-postgresql} \
+    elasticsearch=${COOKIECUTTER_ELASTICSEARCH:-elasticsearch6}
+
+cd "${WORKDIR}/${PROJECT_NAME}"
+
+# Check local installation (this also generates the Pipfile.lock)
+./scripts/bootstrap
+
+# Initialize git in the repository for 'check-manifest' to work
+git init
+git add -A
+
+# Update MANIFEST.in
+pipenv run check-manifest -u || true
+
+# Build application docker images
+./docker/build-images.sh
+# Fire up a full instance via docker-compose.full.yml
+# We will use the services (DB, ES, etc) for running the tests locally
+docker-compose -f docker-compose.full.yml up -d
+./docker/wait-for-services.sh --full
+echo "All services are up."
+
+docker-compose -f docker-compose.full.yml down
+echo "Services successfully stopped"
+
+# Fire up the services we need for testing
+docker-compose up -d
+./docker/wait-for-services.sh
+# Run the instance tests
+# REQUIREMENTS=prod ./run-tests.sh
---
+++ {{cookiecutter.project_shortname}}/docker/build-images.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+#
+# Copyright (C) 2019 CERN.
+# Copyright (C) 2019 Northwestern University.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+script_path=$(dirname "$0")
+
+pipfile_lock_path="$script_path/../Pipfile.lock"
+
+if [ ! -f $pipfile_lock_path ]; then
+    echo "'Pipfile.lock' not found. Generate it by running 'pipenv lock'."
+    exit 1
+fi
+
+# Extract Pipfile.lock hash to use as the docker image tag
+deps_ver=$(cat $pipfile_lock_path | python -c "import json,sys;print(json.load(sys.stdin)['_meta']['hash']['sha256'])")
+
+# Build dependencies image
+docker build -f Dockerfile.base -t {{cookiecutter.project_shortname}}-base:$deps_ver .
+
+# Build application image
+docker build --build-arg DEPENDENCIES_VERSION=$deps_ver . -t {{cookiecutter.project_shortname}}
