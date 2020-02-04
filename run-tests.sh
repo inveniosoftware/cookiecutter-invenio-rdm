427a088897b0b589b55f55afd817939b0bbbadc5
Pablo Panero <ppanero@cern.ch>
2020-02-04 15:52:34 +0100

    s3: add minio container to docker-compose files and adapt scripts for it

--- run-tests.sh
+++ run-tests.sh
@@ -33,7 +33,8 @@ export PROJECT_NAME
 cookiecutter --no-input -o "$WORKDIR" . \
     project_name=${PROJECT_NAME} \
     database=${COOKIECUTTER_DATABASE:-postgresql} \
-    elasticsearch=${COOKIECUTTER_ELASTICSEARCH:-elasticsearch6}
+    elasticsearch=${COOKIECUTTER_ELASTICSEARCH:-elasticsearch6} \
+    file_storage=${COOKIECUTTER_FILE_STORAGE:-local}
 
 PROJECTDIR=${WORKDIR}/${PROJECT_NAME}
 export PROJECTDIR
