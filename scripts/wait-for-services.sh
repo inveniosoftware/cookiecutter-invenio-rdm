78be85edacfe433e73b2092bc353ebb2feb6087f
Sam Arbid <samaa2@kth.se>
2024-10-10 23:53:16 +0200

    search: remove deprecated ElasticSearch support

--- scripts/wait-for-services.sh
+++ scripts/wait-for-services.sh
@@ -6,7 +6,7 @@
 #
 # Copyright (C) 2019-2020 CERN.
 # Copyright (C) 2019-2020 Northwestern University.
-# Copyright (C) 2024 KTH Royal Institute of Technology.
+# Copyright (C) 2024      KTH Royal Institute of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -35,21 +35,21 @@ fi
 if [[ ${COOKIECUTTER_DATABASE} == "mysql" ]]
 then
     _db_check(){ docker compose exec db bash -c "mysql -p${PROJECT_NAME} -e \"select Version();\"" &>/dev/null; }
+    _db_check(){ docker compose exec db bash -c "mysql -p${PROJECT_NAME} -e \"select Version();\"" &>/dev/null; }
     check_ready "MySQL" _db_check
 else
+    _db_check(){ docker compose exec --user postgres db bash -c "pg_isready" &>/dev/null; }
     _db_check(){ docker compose exec --user postgres db bash -c "pg_isready" &>/dev/null; }
     check_ready "Postgres" _db_check
 fi
 
 _search_check(){ curl --output /dev/null --silent --head --fail http://localhost:9200 &>/dev/null; }
-if [[ ${COOKIECUTTER_SEARCH} == "elasticsearch7" ]]
-    check_ready "Elasticsearch" _search_check
-else
-    check_ready "OpenSearch" _search_check
-fi
+check_ready "OpenSearch" _search_check
 
+_redis_check(){ docker compose exec cache bash -c 'redis-cli ping' | grep 'PONG' &> /dev/null; }
 _redis_check(){ docker compose exec cache bash -c 'redis-cli ping' | grep 'PONG' &> /dev/null; }
 check_ready "Redis" _redis_check
 
+_rabbit_check(){ docker compose exec mq bash -c "rabbitmqctl status" &>/dev/null; }
 _rabbit_check(){ docker compose exec mq bash -c "rabbitmqctl status" &>/dev/null; }
 check_ready "RabbitMQ" _rabbit_check
