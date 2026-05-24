6cdee208336195bd1dbd61094d72a33fb21d1a96
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-05-24 21:56:41 +0200

    chore(licenses): update license headers to use SPDX

--- run-tests.sh
+++ run-tests.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-#
-# This file is part of Invenio.
-#
-# Copyright (C) 2019 CERN.
-# Copyright (C) 2019 Northwestern University.
-# Copyright (C) 2024 KTH Royal Institute of Technology.
-#
-# Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# SPDX-FileCopyrightText: 2019 CERN.
+# SPDX-FileCopyrightText: 2019 Northwestern University.
+# SPDX-FileCopyrightText: 2024 KTH Royal Institute of Technology.
+# SPDX-License-Identifier: MIT
 
 # quit on errors:
 set -o errexit
